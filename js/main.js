460d8651ae037b98d8017cf25706e5e12c97da59
jquihano <jeremy.quihano@gmail.com>
2022-03-07 20:54:11 -0800

    Adding more functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let selectionArray = ['rock', 'paper', 'scissors'];
-
 let randomNumber = Math.floor(Math.random()*selectionArray.length);
+
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     if (randomNumber === 0) {
@@ -21,8 +23,20 @@ let computerSelection = computerPlay();
 function playRound(a, b) {
     console.log(b)
     if (a === 'rock' && b === 'scissors') {
+        playerScore++
         return 'You win'
     } 
 }
 
-console.log(playRound(playerSelection, computerSelection))
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    
+    for(let i = 0; i < 5; i++){
+        playRound(playerSelection, computerSelection);
+        console.log(`Player score is ${playerScore}`)
+    }
+    
+}
+
+game();
