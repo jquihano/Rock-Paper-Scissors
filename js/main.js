0291dbf8124be21b09c63fbee45753470391bbfa
jquihano <jeremy.quihano@gmail.com>
2022-03-07 20:42:31 -0800

    Starting functions for RPS game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+let selectionArray = ['rock', 'paper', 'scissors'];
 
+let randomNumber = Math.floor(Math.random()*selectionArray.length);
+
+function computerPlay() {
+    if (randomNumber === 0) {
+        return 'rock'
+    } else if (randomNumber === 1) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+computerPlay();
+
+
+
+let playerSelection = 'rock';
+let computerSelection = computerPlay();
+
+function playRound(a, b) {
+    console.log(b)
+    if (a === 'rock' && b === 'scissors') {
+        return 'You win'
+    } 
+}
+
+console.log(playRound(playerSelection, computerSelection))
