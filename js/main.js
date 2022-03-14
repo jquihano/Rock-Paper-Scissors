8b042ea774c9efead5dcca174c281330d640b81b
jquihano <jeremy.quihano@gmail.com>
2022-03-13 20:34:23 -0700

    Added all functionality for playing RPS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let compScore = 0;
 
 function game() {
     /* Looping 5 times -- playing round 5 times */
-for(let i = 0; i < 5; i++){
+do{
     
 /* Creating random selection for computer */
 let selectionArray = ['Rock', 'Paper', 'Scissors'];
@@ -28,18 +28,20 @@ function computerPlay() {
 /* Playing round function */
 function playRound(player, computer) {
 
-    if(player === "rock" && computer === "scissors") {
+    if(player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper" ) {
         playScore += 1
-        return `Rock beats Paper! You Win!`
-    } else {
+        return `${player} beats ${computer}! You Win!`
+    } else if(player === "rock" && computer === "paper" || player === "paper" && computer === "scissors"  || player === "scissors" && computer === "rock" ){
         compScore += 1
-        return `You Lose!`
+        return `${player} doesn't beat ${computer}! You Lose!`
+    } else {
+        return `It's a tie!`
     }
 }
 
 
         console.log(playRound(playerSelection, computerSelection));
         console.log(`${playScore} ${compScore}`)
-    }
+    } while (playScore < 5 && compScore < 5);
 }
 game();
