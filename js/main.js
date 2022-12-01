355bfffbe154159b2567255810dce86abffbf6e1
Jeremy Quihano <jeremy.quihano@gmail.com>
2022-12-01 10:38:14 -0800

    Added functionality and displaying plays

--- js/main.js
+++ js/main.js
@@ -1,40 +1,67 @@
-let playScore = 0;
-let compScore = 0;
-const buttons = document.querySelectorAll('input')
-    
-/* Creating random selection for computer */
-
-
-/* Setting player & computer selection variables */
-
-//const playerSelection = prompt('Choose your weapon').toLowerCase();
+let playScore = 0
+let compScore = 0
+const buttons = document.querySelectorAll('.input')
+let result = ""
+let champ = ''
 
 /* Computer selection function */
+/* Creating random selection for computer */
 function computerPlay() {
     let selectionArray = ['Rock', 'Paper', 'Scissors'];
     return  selectionArray[Math.floor(Math.random()*selectionArray.length)];
 }
 
-/* Playing round function */
-function playRound(player) {
-    const computer = computerPlay().toLowerCase();
-    if(player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper" ) {
-        playScore += 1
-        console.log(`${player} beats ${computer}! You Win!`)
-    } else if(player === "rock" && computer === "paper" || player === "paper" && computer === "scissors"  || player === "scissors" && computer === "rock" ){
-        compScore += 1
-        console.log(`${player} doesn't beat ${computer}! You Lose!`)
-    } else {
-        return `It's a tie!`
-    }
+function disableButton() {
+    buttons.disabled = 'true';
+}
+
+function winner(){
+    alert(champ)
+    alert('click to play again')
+    playScore = 0;
+    compScore = 0;
 }
 
 
-        //console.log(playRound(playerSelection, computerSelection));
-        console.log(`Player Score: ${playScore} Computer Score:${compScore}`);
+/* Playing round function */
+function playRound(player) {
+    const computer = computerPlay().toLowerCase();
+    if(playScore >= 5) {
+        winner()
+        champ = 'Player wins!'
+        return result
+        
+    } else if(compScore >= 5){
+        champ = 'Comp Wins..'
+        winner()
+        return result
+    }
 
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                playRound(button.value)
-            })
+    if(player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper" ) {
+        playScore += 1
+        result =  `${player} beats ${computer}! You Win!`
+    } else if(player === "rock" && computer === "paper" || player === "paper" && computer === "scissors"  || player === "scissors" && computer === "rock" ){
+        compScore += 1
+        result = `${player} doesn't beat ${computer}! You Lose!`
+        
+    } else {
+        result = `It's a tie!`
+    }
+
+document.querySelector('.display').innerHTML = result
+document.querySelector('.compScore').innerHTML = compScore
+document.querySelector('.playerScore').innerHTML = playScore
+
+return result
+
+}
+
+
+
+//console.log(playRound(playerSelection, computerSelection));
+        
+ buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.value)
         })
+    })
