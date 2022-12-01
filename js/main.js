c3b4e5111e8f6d21aaf4aca3230d6dc12f9d5bde
Jeremy Quihano <jeremy.quihano@gmail.com>
2022-11-30 21:15:34 -0800

    Adding button functionality and logic

--- js/main.js
+++ js/main.js
@@ -1,59 +1,40 @@
 let playScore = 0;
 let compScore = 0;
-
-document.querySelector('#rock').addEventListener('click', playRound)
-document.querySelector('#paper').addEventListener('click', playRound)
-document.querySelector('#scissors').addEventListener('click', playRound)
-
-function playRound(){
-    alert('clicked')
-}
+const buttons = document.querySelectorAll('input')
     
 /* Creating random selection for computer */
-let selectionArray = ['Rock', 'Paper', 'Scissors'];
-let randomNumber = Math.floor(Math.random()*selectionArray.length);
+
 
 /* Setting player & computer selection variables */
 
 //const playerSelection = prompt('Choose your weapon').toLowerCase();
-const computerSelection = computerPlay().toLowerCase();
 
 /* Computer selection function */
 function computerPlay() {
-    if (randomNumber === 0) {
-        return 'Rock'
-    } else if (randomNumber === 1) {
-        return 'Paper'
-    } else {
-        return 'Scissors'
-    }
+    let selectionArray = ['Rock', 'Paper', 'Scissors'];
+    return  selectionArray[Math.floor(Math.random()*selectionArray.length)];
 }
 
 /* Playing round function */
-function playRound(player, computer) {
-
+function playRound(player) {
+    const computer = computerPlay().toLowerCase();
     if(player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper" ) {
         playScore += 1
-        return `${player} beats ${computer}! You Win!`
+        console.log(`${player} beats ${computer}! You Win!`)
     } else if(player === "rock" && computer === "paper" || player === "paper" && computer === "scissors"  || player === "scissors" && computer === "rock" ){
         compScore += 1
-        return `${player} doesn't beat ${computer}! You Lose!`
+        console.log(`${player} doesn't beat ${computer}! You Lose!`)
     } else {
         return `It's a tie!`
     }
 }
 
 
-        console.log(playRound(playerSelection, computerSelection));
+        //console.log(playRound(playerSelection, computerSelection));
         console.log(`Player Score: ${playScore} Computer Score:${compScore}`);
-       
- (playScore < 5 && compScore < 5);
-    if (playScore === 5) {
-            alert(`You Win!`)
-        } else if (compScore === 5) {
-            alert(`You Lose!`)
-        }
 
-
-
-game();
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                playRound(button.value)
+            })
+        })
